d6253a4027061ce45b286ab3ded671d480d1c918
RafaLema <a20163239@pucp.edu.pe>
2019-04-14 13:00:11 -0500

    Pruebe mapas

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,64 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
 import 'package:prueba_gps/User.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main()=>runApp(new AppGPS());
 
 class AppGPS extends StatefulWidget {
   @override
   _AppGPSState createState() => _AppGPSState();
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
 }
+
+class MyApp extends StatefulWidget{
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  // ignore: missing_return
+  Widget build(BuildContext context){
+     return MaterialApp(
+       debugShowCheckedModeBanner: false,
+       home: MyHomePage(),
+     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  GoogleMapController mapController;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            onMapCreated: onMapCreated,
+            initialCameraPosition: CameraPosition(
+                target: LatLng(45.16165, 17.646651),
+                zoom: 15.0,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+
+}
+
 
 class _AppGPSState extends State<AppGPS> {
 
@@ -40,9 +91,20 @@ class _AppGPSState extends State<AppGPS> {
     read();
   }
 
+  void onMapCreated(controller) {
+    setState(() {
+    });
+  }
+
+  void onMapCreated(controller) {
+    setState(() {
+      mapController=controller;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-
+    GoogleMapController mapController;
     return MaterialApp(
       home: Scaffold(
         appBar: new AppBar(title: new Text('PruebaGPS'),),
@@ -57,7 +119,24 @@ class _AppGPSState extends State<AppGPS> {
             });
           },
         ),
-        body: Container(
+        body: Stack(
+          children: <Widget>[
+            GoogleMap(
+              onMapCreated: onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(45.16165, 17.646651),
+                zoom: 15.0,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*body: Container(
           child: Center(
             child: new ListView.builder(
                 itemCount: users.length,
@@ -80,9 +159,4 @@ class _AppGPSState extends State<AppGPS> {
                 },
             ),
           ),
-        )
-      ),
-    );
-  }
-}
-
+        )*/
